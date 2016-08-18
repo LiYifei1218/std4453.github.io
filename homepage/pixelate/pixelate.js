1503bac5833b137333e4ed85ee85892a43ac06dc
std4453 <std4453@foxmail.com>
2016-08-18 13:54:19 +0800

    Directory opimization

--- homepage/pixelate/pixelate.js
+++ homepage/pixelate/pixelate.js
@@ -4,6 +4,7 @@ window.pixelate = pixelate;
 pixelate.defaultConf = {
 	pixel: [4, 4],
 	interval: [5, 5],
+	bg: 0x000000,
 };
 
 function initPixelate(context, conf) {
@@ -15,6 +16,8 @@ function initPixelate(context, conf) {
 
 	THREE.Vector2.prototype.set.apply(uniforms['pixel'].value, conf.pixel);
 	THREE.Vector2.prototype.set.apply(uniforms['interval'].value, conf.interval);
+	THREE.Vector3.prototype.set.apply(uniforms['bg'].value, [((conf.bg & 0xFF0000) >> 16) / 256,
+		((conf.bg & 0x00FF00) >> 8) / 256, ((conf.bg & 0x0000FF) >> 0) / 256]);
 
 	uniforms['size'].value.set(width, height);
 	uniforms['texel'].value.set(1 / width, 1 / height);
